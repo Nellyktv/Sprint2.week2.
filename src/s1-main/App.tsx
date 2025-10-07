c107870f8007fa4bab194fedfaaa52d405b032f4
Elena Diatlova <elena.diatlova@5dhub.tech>
2025-10-07 15:36:54 +0300

    HW3

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,7 +1,8 @@
 
  import s from './App.module.css'
 import '../fonts/Montserrat/fonts.css';
-import HW2 from '../s2-homeworks/hw02/HW2';
+import HW3 from '../s2-homeworks/hw03/HW3';
+
 
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
         <div className={s.App}>
             {/* <HW1/> */}
             {/*раскомментировать по ходу выполнения*/}
-            <HW2/>
-            {/*<HW3/>*/}
+            {/* <HW2/> */}
+            <HW3/>
             {/*<HW4/>*/}
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -21,9 +21,7 @@ function Affair(props: AffairPropsType) {
   return (
     <div id={'hw2-affair-' + props.affair._id} className={affairClass}>
       <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-        {/*создаёт студент*/}
-
-        {/**/}
+        name
       </div>
       <div id={'hw2-priority-' + props.affair._id} hidden>
         {props.affair.priority}
